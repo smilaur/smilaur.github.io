a8e90cc9f44f897c30c7755472f6aad6c4c18366
BSD-06 <walker.alice.e@gmail.com>
2017-01-06 15:31:10 +0800

    praise helix

---
+++ omantyeshrine/js/index.js
@@ -0,0 +1,19 @@
+var oma1 = document.getElementById("oman1");
+var oma2 = document.getElementById("oman2");
+var oma3 = document.getElementById("oman3");
+var captionBox = document.getElementById("caption");
+
+oma1.addEventListener("mouseover", function() {
+  //this is the code that runs when oma1 is clicked
+  captionBox.innerHTML = "PRAISE HELIX!";
+});
+
+oma2.addEventListener("mouseover", function() {
+  //this is the code that runs when oma1 is clicked
+  captionBox.innerHTML = "PRAISE HELIX!!";
+});
+
+oma3.addEventListener("mouseover", function() {
+  //this is the code that runs when oma1 is clicked
+  captionBox.innerHTML = "PRAISE HELIX!!!";
+});
